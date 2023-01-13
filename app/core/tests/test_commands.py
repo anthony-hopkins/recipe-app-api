c3090d2d5b73f93f6778a1365af23e77447c6f99
Anthony Hopkins <anthony.hopkins@yorkspacesystems.com>
2023-01-12 18:03:18 -0700

    Fixed PEP8 lint errors.

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -9,7 +9,7 @@ from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
 
-#import core.management.commands.wait_for_db
+# import core.management.commands.wait_for_db
 
 
 @patch('core.management.commands.wait_for_db.Command.check')
